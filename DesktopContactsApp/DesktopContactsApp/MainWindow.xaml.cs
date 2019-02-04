93b600b062591055db6fc2219177f80488f16fb7
rjDevOnRun <34760817+rjDevOnRun@users.noreply.github.com>
2019-02-04 10:18:57 +0900

    Add files via upload

--- DesktopContactsApp/DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/DesktopContactsApp/MainWindow.xaml.cs
@@ -21,9 +21,12 @@ namespace DesktopContactsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
         public MainWindow()
         {
             InitializeComponent();
+
+            contacts = new List<Contact>();
 
             ReadDatabase();
         }
@@ -41,8 +44,27 @@ namespace DesktopContactsApp
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
-                var contacts = conn.Table<Contact>().ToList();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
             }
+
+            if(contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            var filteredList2 = (from c2 in contacts
+                                where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+                                orderby c2.Email
+                                select c2).ToList();
+
+            contactsListView.ItemsSource = filteredList;
         }
     }
 }
